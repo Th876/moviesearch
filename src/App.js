b9ebf8584041f8ed40d2b4db3483b9e53791a5f8
TH876 <tayhiggins14@gmail.com>
2023-04-15 00:24:18 -0400

    Adding lecture example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import "./App.css";
+// WE IMPORT OUR COMPONENTS
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
 
-function App() {
+export default function App() {
+  //variable with your apiKey
+  const apiKey = "98e3fb1f";
+
+  //State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  //Function to getMovies
+  const getMovie = async (searchTerm) => {
+    // make fetch request and store response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a javascript object
+    const data = await response.json();
+    //set the Movie state to the movie
+    setMovie(data);
+  };
+
+  //This will run on the first render but not on subsquent renders
+  //So useEffect will prevent 'No Movie to Display' from showing up on the first render. 
+  //the bracket[] means what change we're looking for; if this variable updates
+  //useEffect(<function>, <dependency>)
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  // USE OUR COMPONENTS IN APPs RETURNED JSX
+  // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
 
-export default App;
+;
